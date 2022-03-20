7a841fc32df7b769f4fc1fe239a7ac47be6a6a93
Aagam Jain <aagamjain573@gmail.com>
2022-03-20 08:47:14 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose =require('mongoose');
 require('dotenv').config();
 function connectdb(){
-    mongoose.connect("mongodb+srv://getlost01:yVe7jIn5R5eMMGjH@cluster0.auyyt.mongodb.net/linkshare?retryWrites=true&w=majority", {useNewUrlParser: true ,useUnifiedTopology: true});
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser: true ,useUnifiedTopology: true});
     const connection=mongoose.connection;
     try{
         connection.once('open',()=>{
