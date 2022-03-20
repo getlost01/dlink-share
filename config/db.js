c16893246e5b2c6415a23c9159da2989c67cae8e
AAGAM JAIN <aagamjain573@gmail.com>
2022-03-20 08:38:42 +0530

    moongoose

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose =require('mongoose');
 require('dotenv').config();
 function connectdb(){
-    mongoose.connect(process.env.MONGO_CONNECTION_URl, {useNewUrlParser: true ,useUnifiedTopology: true});
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser: true ,useUnifiedTopology: true});
     const connection=mongoose.connection;
     try{
         connection.once('open',()=>{
